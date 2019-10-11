9d0aa70f9919c9de27ba13bc2f5055bdf4d2e3a6
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-11 21:52:54 +0530

    Update novalnet_invoice.js

--- resources/js/novalnet_invoice.js
+++ resources/js/novalnet_invoice.js
@@ -1,4 +1,21 @@
 $(document).ready( function() {
+	$( function() {
+	  let current_date = new Date();
+	  current_date.getFullYear();
+	  var max_year = current_date.getFullYear() - 18;	  
+	  var min_year = current_date.getFullYear() - 60;
+	  console.log(max_year);
+		 console.log(min_year);
+    var year_range = [];
+    
+    for( var year = max_year; year >= min_year; year-- ) {				
+		year_range.push('' + year + '');
+	}
+	
+    $( "#nn_month" ).autocomplete({
+      source: year_range
+    });
+  } );
 	
 	$('#nn_invoice_form').on('submit', function() {
 		$('#novalnet_form_btn').attr('disabled',true);

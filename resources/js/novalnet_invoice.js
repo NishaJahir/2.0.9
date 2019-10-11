e55d6c38c6e32cc82f3f6cad8bb0ee2356febd44
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-11 21:15:09 +0530

    Update novalnet_invoice.js

--- resources/js/novalnet_invoice.js
+++ resources/js/novalnet_invoice.js
@@ -1,13 +1,4 @@
 $(document).ready( function() {
-	console.log('nnnnn');
-
-	$ (function() {
-    var Months = ["January", "Febraury", "March", "April", "Jan", "Feb", "Test"];
-    $("#nn_month").autocomplete({
-     // source: Months
-     console.log('jjj');
-    });
-  } );
 	
 	$('#nn_invoice_form').on('submit', function() {
 		$('#novalnet_form_btn').attr('disabled',true);

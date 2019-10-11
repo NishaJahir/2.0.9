029374feaa79256713720d41380dc135e97c1da2
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-11 17:25:45 +0530

    Add files via upload

---
+++ resources/js/novalnet_invoice.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+	$( function() {
+    var Months = ["January", "Febraury", "March", "April", "Jan", "Feb", "Test"];
+    $( "#nn_month" ).autocomplete({
+      source: Months
+    });
+  } );
+	
+	$('#nn_invoice_form').on('submit', function() {
+		$('#novalnet_form_btn').attr('disabled',true);
+	});
+});

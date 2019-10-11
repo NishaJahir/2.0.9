c42c185cde36e36ad717bc10fe983dc925b8bda5
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-11 22:01:04 +0530

    Update novalnet_invoice.js

--- resources/js/novalnet_invoice.js
+++ resources/js/novalnet_invoice.js
@@ -21,3 +21,13 @@ $(document).ready( function() {
 		$('#novalnet_form_btn').attr('disabled',true);
 	});
 });
+
+ function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }

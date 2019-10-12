b0f428f5e85d639a18b86c68665f60c8742e213d
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-12 12:33:10 +0530

    Update novalnet_invoice.js

--- resources/js/novalnet_invoice.js
+++ resources/js/novalnet_invoice.js
@@ -27,6 +27,14 @@ $(document).ready( function() {
     });
   } );
 	
+	var NotificationService = require('services/NotificationService');
+         NotificationService.success('Sie wurden erfolgreich ausgeloggt.').closeAfter(3000);
+
+
+
+
+
+	
 	$('#nn_invoice_form').on('submit', function() {
 		$('#novalnet_form_btn').attr('disabled',true);
 	});
